72198777c5d25f627af9002ff66e527f223196b1
suay-selay <suay_999@hotmail.com>
2020-12-07 14:21:21 +0100

    Test clas för Contact

---
+++ src/test/java/com/domänobjekt/ContactTest.java
@@ -0,0 +1,16 @@
+package com.domänobjekt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPhoneNumber() {
+    }
+}
