eee76704aaae5e00df1d361dc97fb81c99c5f7f2
nicofel <nico_feldt@hotmail.com>
2020-12-07 11:00:42 +0100

    Added test file

--- src/test/java/com/TeamEffort/MenuTest.java
+++ src/test/java/com/TeamEffort/MenuTest.java
@@ -1,12 +1,21 @@
 package com.TeamEffort;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MenuTest {
 
-    @Test
-    void main() {
+    @BeforeAll
+    public static void BeforeAll(){
+        System.out.println("Starting test");
     }
+
+@Test
+    public static void Test(){
+    System.out.println("Test");
+}
+
+
 }
