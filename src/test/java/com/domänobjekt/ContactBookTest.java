83a2eeb6e8dad40e3888a1c3ad54c4e013a1f3f2
Yonas Romell <yonas.romell@live.se>
2020-12-07 10:29:08 +0100

    Added test for ContactBook

---
+++ src/test/java/com/domänobjekt/ContactBookTest.java
@@ -0,0 +1,31 @@
+package com.domänobjekt;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactBookTest {
+
+    @Test
+    void addNewContact() {
+    }
+
+    @Test
+    void removeContact() {
+    }
+
+    @Test
+    void queryContact() {
+    }
+
+    @Test
+    void printContact() {
+    }
+
+    @Test
+    void addFavourite() {
+    }
+
+    @Test
+    void printFavouriteContact() {
+    }
+}
