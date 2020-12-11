c25e7d79d54869a1ebcfffa70d05584b9f9aeb66
nicofel <nico_feldt@hotmail.com>
2020-12-11 10:38:36 +0100

    Removed tests

--- src/test/java/com/TeamEffort/MenuTest.java
+++ src/test/java/com/TeamEffort/MenuTest.java
@@ -6,26 +6,4 @@ import org.junit.jupiter.api.Test;
 
 class MenuTest {
 
-   @BeforeAll
-    public static void BeforeAll(){
-        System.out.println("Starting test");
-   }
-
-
-@Test
-    public void searchContact() {
-    Assertions.assertThrows(NumberFormatException.class, () -> {
-        Integer.parseInt("One");
-    });
-    System.out.println("The test passed");
-}
-       //menuTest.searchContact();
-       //System.out.println("Test");
-
-/*@Test
-    void addNewContact(){
-   System.out.println("Test");
-}*/
-
-
 }
